dcfe41d551bfaa15e12bcca86fa0b8a693913d51
Maaz <maazshaikh4559@gmail.com>
2022-08-10 17:50:53 +0530

    Added skills and projects

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from "./components/Navbar";
 import Portfolio from "./components/Portfolio";
 import Skills from "./components/Skills";
 
+
 function App() {
   // Parallax effect
   const [offset, setOffset] = useState(0);
