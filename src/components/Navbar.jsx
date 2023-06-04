79037e187477431d7b77b856819646419e5fa972
Maaz <maazshaikh4559@gmail.com>
2023-06-04 17:02:59 +0530

    Added navbar UI for large screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,16 +5,16 @@ function Navbar(props) {
   return (
     <>
       <nav
-        className={`fixed  pt-8 w-2/3 md:w-1/2 top-0 lg:hidden rounded-r-xl bg-gradient-to-tr ${
+        className={`fixed  pt-8 lg:p-0 lg:m-0 w-2/3 md:w-1/2 lg:w-min top-0 lg:top-[17vh] lg:left-0 rounded-r-xl bg-gradient-to-tr ${
           props.mode == "light"
             ? "from-blue-500 to-cyan-100"
             : "from-blue-900 to-violet-900 "
-        } min-h-full overflow-hidden duration-700 z-10 ${
-          props.navDisplay ? "" : "-translate-x-full"
+        } min-h-full lg:min-h-0 lg:h-2/3 lg:flex lg:items-center overflow-hidden duration-700 z-10 ${
+          props.navDisplay ? "" : "-translate-x-full lg:translate-x-0"
         }`}
       >
         <div className="container">
-          <div className="data relative py-4 md:py-14 mx-10">
+          <div className="data lg:hidden relative py-4 md:py-14 mx-10">
             <div
               className={`mask justify-center items-end ${
                 props.mode === "light" ? "text-sky-900" : "text-cyan-200"
@@ -35,39 +35,39 @@ function Navbar(props) {
           <ul
             className={`navlist font-bold text-sm ${
               props.mode === "light" ? "text-sky-900" : "text-cyan-200"
-            } list-none w-18 mt-8 flex flex-col  text-center space-y-7`}
+            } list-none w-18 mt-8 lg:m-4 lg:p-0  flex flex-col  text-center lg:text-left space-y-7`}
           >
             <li className="navItem">
               <Link to="/" className="navLink">
                 <i className="bx bx-home hidden md:inline"></i>
-                <p>Home</p>
+                <p className="lg:hidden">Home</p>
               </Link>
             </li>
             <li className="navItem">
               <Link to="/about" className="navLink">
                 <i className="bx bx-user hidden md:inline"></i>
-                <p>About Me</p>
+                <p className="lg:hidden">About Me</p>
               </Link>
             </li>
             <li className="navItem">
               <Link to="/portfolio" className="navLink">
                 <i className="bx bx-briefcase hidden md:inline"></i>
-                <p>Portfolio</p>
+                <p className="lg:hidden">Portfolio</p>
               </Link>
             </li>
             <li className="navItem">
               <Link to="/skills" className="navLink">
                 <i className="bx bx-bookmark hidden md:inline"></i>
-                <p>Skills</p>
+                <p className="lg:hidden">Skills</p>
               </Link>
             </li>
             <li className="navItem">
               <Link to="/contact" className="navLink">
                 <i className="bx bxs-chat hidden md:inline"></i>
-                <p>Contact Me</p>
+                <p className="lg:hidden">Contact Me</p>
               </Link>
             </li>
-            <ul className="socials flex justify-center p-7">
+            <ul className="socials flex justify-center lg:text-left lg:flex-col p-7 lg:p-0 lg:pt-10">
               <li>
                 <a
                   className="bx bxl-facebook cursor-pointer hover:text-3xl"
