9af993b714b3aa419b65ded60cf47904a22efd81
Maaz <maazshaikh4559@gmail.com>
2023-06-08 15:05:30 +0530

    Adjusted projects font-size

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -8,26 +8,26 @@ function Project(props) {
         className={`group p-5 bg-neutral-900 border-4 shadow-2xl rounded-xl min-w-fit `}
       >
         <div
-          className={`project-name p-3 text-center text-xl font-bold ${
+          className={`project-name p-3 text-center font-bold ${
             props.mode === "dark" ? "bg-neutral-800" : "bg-neutral-100"
           } rounded-lg`}
         >
           {props.projectName}
         </div>
-        <div className="about relative w-fit pl-1 pr-12 text-lg h-0 hidden text-neutral-300 top-10 left-7 group-hover:block">
+        <div className="about relative w-fit pl-1 pr-12 h-0 hidden text-neutral-300 top-10 left-7 group-hover:block">
           {props.about}
         </div>
         <img
           src={props.img}
-          className=" w-80 h-64 mx-auto m-5 rounded-xl group-hover:opacity-5 duration-300 object-cover"
+          className="h-56 mx-auto m-5 rounded-xl group-hover:opacity-5 duration-300 object-cover"
         />
         <a href={props.preview} target="_blank">
-          <button className="py-2 px-10 m-2 rounded-full block mx-auto bg-neutral-100 text-lg text-black hover:bg-blue-200">
+          <button className="py-2 px-10 m-2 rounded-full block mx-auto bg-neutral-100  text-black hover:bg-blue-200">
             Live Preview
           </button>
         </a>
         <a href={props.code} target="_blank">
-          <button className="py-2 px-10 m-2 rounded-full block mx-auto bg-neutral-100 text-lg text-black hover:bg-red-200">
+          <button className="py-2 px-10 m-2 rounded-full block mx-auto bg-neutral-100 text-black hover:bg-red-200">
             Source code
           </button>
         </a>
